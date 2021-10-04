c22353f967203196db02c0bb164919716180acf7
Ree3a <riyastha406@gmail.com>
2021-10-04 08:29:15 +0545

    adding database operation

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -105,7 +105,7 @@ public class LoginForm implements ActionListener{
                                         if (adminRS.next()){
                                                 JOptionPane.showMessageDialog(login_frame,"Login Successful as Admin");
                                                 login_frame.dispose();
-                                                new AdminDashboard();
+//                                                new AdminDashboard();
                                         }else if (adminName.length()==0 || adminPassword.length()==0){
                                                 JOptionPane.showMessageDialog(btn_login, "Please fill both the details");
                                         }
@@ -127,7 +127,7 @@ public class LoginForm implements ActionListener{
                                         if (rs.next()){
                                                 JOptionPane.showMessageDialog(login_frame,"Login Successful");
                                                 login_frame.dispose();
-                                                new UserDashboard(name);
+//                                                new UserDashboard(name);
                                         }else if (name.length()==0 || pass.length()==0){
                                                 JOptionPane.showMessageDialog(btn_login, "Please fill both the details");
                                         }
---
+++ src/User.java
@@ -0,0 +1,68 @@
+public class User {
+    private String username;
+    private String mobileNumber;
+    private String userEmail;
+    private String userGender;
+    private String userDob;
+    private String userAddress;
+    private String password1;
+    private String password2;
+    public User(String username, String mobileNumber, String userEmail, String userGender, String userDob, String userAddress, String password1, String password2) {
+        this.username = username;
+        this.mobileNumber = mobileNumber;
+        this.userEmail = userEmail;
+        this.userGender = userGender;
+        this.userDob = userDob;
+        this.userAddress = userAddress;
+        this.password1 = password1;
+        this.password2 = password2;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+    public String getUserEmail() {
+        return userEmail;
+    }
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+    public String getUserGender() {
+        return userGender;
+    }
+    public void setUserGender(String userGender) {
+        this.userGender = userGender;
+    }
+    public String getUserDob() {
+        return userDob;
+    }
+    public void setUserDob(String userDob) {
+        this.userDob = userDob;
+    }
+    public String getUserAddress() {
+        return userAddress;
+    }
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+    public String getPassword1() {
+        return password1;
+    }
+    public void setPassword1(String password1) {
+        this.password1 = password1;
+    }
+    public String getPassword2() {
+        return password2;
+    }
+    public void setPassword2(String password2) {
+        this.password2 = password2;
+    }
+}
